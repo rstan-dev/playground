82c83d1741d04cf073d60a61e0417fcec389e277
Russ-S <rstanleym74@gmail.com>
2023-02-06 17:51:09 +0000

    JS: createList(), drag and drop functions, swapItems() and drag eventListeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,7 +75,7 @@ function updateLS() {
 // P2: Draggable List functionality
 const draggable_list = document.getElementById('draggable-list');
 
-const richestpeople = [
+const richestPeople = [
     'Jeff Bezos',
     'Bill Gates',
     'Warren Buffet',
@@ -84,7 +84,7 @@ const richestpeople = [
     'Michael Bloomberg',
 ];
 
-// P2: Store listItems
+// P2: Store list Items
 const listItems = [];
 
 let dragStartIndex;
@@ -95,6 +95,82 @@ createList();
 function createList() {
     [...richestPeople]
     .forEach((person, index) => {
-        const listItem =
-    })
+        const listItem = document.createElement('li');
+
+        listItem.setAttribute('data-index', index);
+
+        listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+          <p class="person-name">${person}</p>
+          <i class="fas fa-grip-lines"></i>
+        </div>
+        `;
+
+        listItems.push(listItem);
+
+        draggable_list.appendChild(listItem);
+    });
+
+    addEventListeners();
+};
+
+// P2 Drag & Drop functions
+
+function dragStart() {
+    // console.log('Event:', 'dragstart');
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+
 }
+
+function dragEnter() {
+    // console.log('Event:', 'dragenter');
+    this.classList.add('over');
+}
+
+function dragOver(e) {
+    // console.log('Event:', 'dragover');
+    e.preventDefault();
+}
+
+function dragDrop() {
+    //  console.log('Event:', 'drop');
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex);
+
+    this.classList.remove('over');
+}
+
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
+
+}
+
+
+function dragLeave() {
+    // console.log('Event:', 'dragleave');
+    this.classList.remove('over');
+}
+
+
+
+function addEventListeners() {
+    const draggables = document.querySelectorAll('.draggable');
+    const dragListItems = document.querySelectorAll('.draggable-list li');
+
+    draggables.forEach(draggable => {
+        draggable.addEventListener('dragstart', dragStart);
+    })
+
+    dragListItems.forEach(item => {
+        item.addEventListener('dragover', dragOver);
+        item.addEventListener('drop', dragDrop);
+        item.addEventListener('dragenter', dragEnter);
+        item.addEventListener('dragleave', dragLeave);
+    })
+
+};
