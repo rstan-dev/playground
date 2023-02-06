0ccf8ec2902ab2041dac690c8f708dfdcb6067a5
Russ-S <rstanleym74@gmail.com>
2023-02-06 15:53:50 +0000

    Start sortable list functionality, add basic P2 HTML and setup JS variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
-// To Do app functionality
+// P1: To Do app functionality
 
 const todoForm = document.getElementById('todo-form');
 const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todos');
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if (todos) {
+    todos.forEach(todo => addTodo(todo));
+};
+
+const doneButton = document.getElementsByClassName('done');
 
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -25,8 +33,68 @@ function addTodo(todo) {
 
         todoEl.innerText = todoText;
 
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed');
+            updateLS();
+        });
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+            updateLS();
+
+        });
+
+        // doneButton.addEventListener('click', => todoEl.classList.toggle('completed'));
+
         todoList.appendChild(todoEl);
 
         todoInput.value = '';
+
+        updateLS();
     }
 };
+
+function updateLS() {
+    todosEl = document.querySelectorAll('li');
+
+    const todos = [];
+
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed')
+        });
+    })
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+//-----------------------------------------------------
+// P2: Draggable List functionality
+const draggable_list = document.getElementById('draggable-list');
+
+const richestpeople = [
+    'Jeff Bezos',
+    'Bill Gates',
+    'Warren Buffet',
+    'Mark Zuckerberg',
+    'Larry Page',
+    'Michael Bloomberg',
+];
+
+// P2: Store listItems
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+//P2: Insert list items into DOM
+function createList() {
+    [...richestPeople]
+    .forEach((person, index) => {
+        const listItem =
+    })
+}
