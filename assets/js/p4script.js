c14699824dd8a5b68d97a27a0ecf07d062704f1f
Russ-S <rstanleym74@gmail.com>
2023-02-08 15:48:19 +0000

    add toDoList Function - that adds a task, clicks the check box and toggles the checked class on and off

--- assets/js/p4script.js
+++ assets/js/p4script.js
@@ -1,1 +1,62 @@
 //from Udemy course: https:www.udemy.com/course/html-css-javascript-projects-for-beginners/learn/lecture/34705910#overview
+
+const formEl = document.querySelector(".form");
+const inputEl = document.querySelector(".input");
+const ulEl = document.querySelector(".list");
+
+
+
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    // console.log(inputEl.value)  check ist working
+
+    toDoList();
+
+})
+
+function toDoList() {
+    let newTask = inputEl.value;
+
+    const liEl = document.createElement("li");
+
+    liEl.innerText = newTask;
+
+    ulEl.appendChild(liEl);
+    inputEl.value = "";
+
+    const checkBtnEl = document.createElement("div");
+    checkBtnEl.innerHTML = `
+        <i class="fa-regular fa-square"></i>  
+    `;
+    liEl.appendChild(checkBtnEl);
+
+    const trashBtnEl = document.createElement("div");
+    trashBtnEl.innerHTML = `
+        <i class="fa-solid fa-trash-can"></i>
+    `;
+    liEl.appendChild(trashBtnEl);
+
+    checkBtnEl.addEventListener("click", () => {
+        liEl.classList.toggle("checked");
+
+        if (liEl.className === "checked") {
+            checkBtnEl.innerHTML = `
+        <i class="fa-sharp fa-solid fa-square-check"></i>  
+    `;
+        } else {
+            checkBtnEl.innerHTML = `
+            <i class="fa-regular fa-square"></i>  
+    `;
+        };
+
+    });
+
+    trashBtnEl.addEventListener("click", () => {
+        liEl.remove();
+    });
+
+}
+
+//______________________________________________________
