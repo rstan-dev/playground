a56488fc3df344d9116068818a5bbc22cde8cbbf
Sean Knowles <s@seanknowles.dev>
2023-02-09 23:39:45 +0000

    fix: fixed issues and seperated work from last night ;)

---
+++ assets/js/p4script(Sean).js
@@ -0,0 +1,146 @@
+//from Udemy course: https:www.udemy.com/course/html-css-javascript-projects-for-beginners/learn/lecture/34705910#overview
+//toggling 2 icons - https://stackoverflow.com/questions/46625249/toggling-innerhtml-in-javascript
+
+const formEl = document.querySelector(".form");
+const inputEl = document.querySelector(".input");
+const ulEl = document.querySelector(".list");
+
+const onPageLoad = renderList(getLocalStorage());
+onPageLoad();
+
+/////////////////////////
+// Event Listeners - responsible for listening for events and calling functions
+/////////////////////////
+
+/**
+ * Function is called when the form is submitted
+ * it creates a new list item object and adds it to local storage
+ * it then calls the renderList function to re-render the list to the page
+ * it also calls the clearInput function to clear the input field
+ * @param {*} event
+ * @returns {void}
+ */
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newItem = {
+    id: Date.now(),
+    title: inputEl.value,
+    checked: false,
+  };
+
+  addListItem(newItem);
+  renderList(getLocalStorage());
+  clearInput();
+});
+
+/////////////////////////
+// Render Functions - responsible for rendering the list and items to the page
+/////////////////////////
+
+/**
+ * Function renders a list of items to the page
+ * for each item in the list, it calls the listItem function
+ * @param {*} list
+ * @returns {void}
+ */
+function renderList(list) {
+  ulEl.innerHTML = "";
+  list.forEach((task) => {
+    listItem(task);
+  });
+}
+
+/**
+ * Function renders a single list item to the page
+ * it creates the li element, adds the text, and adds the buttons
+ * it also adds event listeners to the buttons to toggle the checked state
+ * @params task
+ * @returns {void}
+ */
+function listItem(task) {
+  const liEl = document.createElement("li");
+  ulEl.appendChild(liEl);
+
+  liEl.innerText = task.title;
+
+  const checkBtnEl = document.createElement("div");
+
+  task.checked
+    ? (checkBtnEl.innerHTML = `<i class="fa-solid fa-check-square"></i>`)
+    : (checkBtnEl.innerHTML = `<i class="fa-regular fa-square"></i>`);
+
+  liEl.appendChild(checkBtnEl);
+
+  const trashBtnEl = document.createElement("div");
+  trashBtnEl.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  liEl.appendChild(trashBtnEl);
+
+  checkBtnEl.addEventListener("click", () => {
+    liEl.classList.toggle("checked");
+    if (task.checked) {
+      task.checked = false;
+      checkBtnEl.innerHTML = `<i class="fa-regular fa-square"></i>`;
+    } else {
+      task.checked = true;
+      checkBtnEl.innerHTML = `<i class=fa-sharp fa-solid fa-square-check"></i>`;
+    }
+    updateListItemById(task.id, task);
+  });
+
+  trashBtnEl.addEventListener("click", () => {
+    liEl.remove();
+    removeListItemById(task.id);
+  });
+}
+
+/////////////////////////
+// Local Storage Functions - responsible for interacting with local storage
+/////////////////////////
+
+// get list items from local storage
+function getLocalStorage() {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+}
+
+// get list item from local storage by id
+function getListItemById(id) {
+  const list = getLocalStorage();
+  return list.find((item) => item.id === id);
+}
+
+// add list item to local storage
+function addListItem(item) {
+  const list = getLocalStorage();
+  list.push(item);
+  localStorage.setItem("list", JSON.stringify(list));
+}
+
+// remove list items from local storage
+function removeListItemById(id) {
+  const list = getLocalStorage();
+  const updatedList = list.filter((item) => item.id !== id);
+  localStorage.setItem("list", JSON.stringify(updatedList));
+}
+
+// update list item in local storage by id
+function updateListItemById(id, updatedItem) {
+  const list = getLocalStorage();
+  const updatedList = list.map((item) => {
+    if (item.id === id) {
+      return updatedItem;
+    }
+    return item;
+  });
+  localStorage.setItem("list", JSON.stringify(updatedList));
+}
+
+/////////////////////////
+// Helper Functions - responsible for helping other functions
+/////////////////////////
+
+function clearInput() {
+  inputEl.value = "";
+}
--- assets/js/p4script.js
+++ assets/js/p4script.js
@@ -8,92 +8,66 @@ const ulEl = document.querySelector(".list");
 let list = JSON.parse(localStorage.getItem("list"));
 console.log(list);
 
-list.forEach(task => {
-    toDoList(task);
-})
-
-
-
+list.forEach((task) => toDoList(task));
 
 formEl.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    // console.log(inputEl.value)  check ist working
-
-    toDoList();
-
-})
+  event.preventDefault();
+  toDoList();
+});
 
 function toDoList(task) {
-    let newTask = inputEl.value;
+  const liEl = document.createElement("li");
+  let newTask = inputEl.value;
 
-    if (task) {
-        newTask = task.name
-    }
+  if (task) newTask = task.name;
+  if (task && task.checked) liEl.classList.add("checked");
 
-    const liEl = document.createElement("li");
+  liEl.innerText = newTask;
 
-    if (task && task.checked) {
-        liEl.classList.add("checked");
-    };
+  ulEl.appendChild(liEl);
+  inputEl.value = "";
 
+  const checkBtnEl = document.createElement("div");
 
-    liEl.innerText = newTask;
+  liEl.className === "checked"
+    ? (checkBtnEl.innerHTML = `<i class="fa-sharp fa-solid fa-square-check"></i>`)
+    : (checkBtnEl.innerHTML = `<i class="fa-regular fa-square"></i>`);
 
-    ulEl.appendChild(liEl);
-    inputEl.value = "";
+  liEl.appendChild(checkBtnEl);
 
-    const checkBtnEl = document.createElement("div");
-    checkBtnEl.innerHTML = `
-        <i class="fa-regular fa-square"></i>  
-    `;
-    liEl.appendChild(checkBtnEl);
+  const trashBtnEl = document.createElement("div");
+  trashBtnEl.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
+  liEl.appendChild(trashBtnEl);
 
-    const trashBtnEl = document.createElement("div");
-    trashBtnEl.innerHTML = `
-        <i class="fa-solid fa-trash-can"></i>
-    `;
-    liEl.appendChild(trashBtnEl);
-
-    checkBtnEl.addEventListener("click", () => {
-        liEl.classList.toggle("checked");
-        updateLocalStorage();
-
-        if (liEl.className === "checked") {
-            checkBtnEl.innerHTML = `
-        <i class="fa-sharp fa-solid fa-square-check"></i>  
-    `;
-        } else {
-            checkBtnEl.innerHTML = `
-            <i class="fa-regular fa-square"></i>  
-    `;
-        };
-
-    });
-
-    trashBtnEl.addEventListener("click", () => {
-        liEl.remove();
-        updateLocalStorage();
-    });
-
+  checkBtnEl.addEventListener("click", () => {
+    liEl.classList.toggle("checked");
     updateLocalStorage();
 
+    liEl.className === "checked"
+      ? (checkBtnEl.innerHTML = `<i class="fa-sharp fa-solid fa-square-check"></i>`)
+      : (checkBtnEl.innerHTML = `<i class="fa-regular fa-square"></i>`);
+  });
+
+  trashBtnEl.addEventListener("click", () => {
+    liEl.remove();
+    updateLocalStorage();
+  });
+
+  updateLocalStorage();
 }
 
 //______________________________________________________
 
 function updateLocalStorage() {
-    const liEls = document.querySelectorAll("li");
-    list = [];
+  const liEls = document.querySelectorAll("li");
+  list = [];
 
-    liEls.forEach(liEl => {
-        list.push({
-            name: liEl.innerText,
-            checked: liEl.classList.contains("checked")
-        });
+  liEls.forEach((liEl) => {
+    list.push({
+      name: liEl.innerText,
+      checked: liEl.classList.contains("checked"),
     });
+  });
 
-    localStorage.setItem("list", JSON.stringify(list))
-
-
+  localStorage.setItem("list", JSON.stringify(list));
 }
